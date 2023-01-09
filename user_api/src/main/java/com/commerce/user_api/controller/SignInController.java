7a27abdb2ef6ec0eadd5cef81bfcb54737efc33a
IndeokDukeSeo <dukeindream@gmail.com>
2023-01-09 17:52:16 +0900

    Refactor (1) scratch_user.http 작성 - 데이터 CRUD 테스트 (2) 일부 html 컨벤션 수정

--- user_api/src/main/java/com/commerce/user_api/controller/SignInController.java
+++ user_api/src/main/java/com/commerce/user_api/controller/SignInController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/signIn")
+@RequestMapping(value = "/signin")
 @RequiredArgsConstructor
 public class SignInController {
     private final SignInApplication signInApplication;
